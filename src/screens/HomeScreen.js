86241c5be23ea0202ff795bb4d0112809929ce9f
adriaanbd <adriaanbd@gmail.com>
2020-04-14 23:33:55 -0500

    logout and handleLogout in Home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Text} from 'react-native-elements';
+import {Text, Button} from 'react-native-elements';
+import {useDispatch} from 'react-redux';
+import {logoutUser} from '../store/thunks/user';
 
-const HomeScreen = () => (
-  <View>
-    <Text>HOME SCREEN</Text>
-  </View>
-);
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => dispatch(logoutUser());
+
+  return (
+    <View>
+      <Text>HOME SCREEN</Text>
+      <Button title="Logout" onPress={handleLogout} />
+    </View>
+  );
+};
 
 export default HomeScreen;
