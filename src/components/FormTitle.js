45dee83448727bf5e161f06436156dc6acc5c3fc
adriaanbd <adriaanbd@gmail.com>
2020-04-22 00:29:31 -0500

    adding FormTitle component

---
+++ src/components/FormTitle.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text} from 'react-native-elements';
+
+const FormTitle = ({title1, title2}) => {
+  return (
+    <View style={styles.titleBox}>
+      <Text style={styles.title}>{title1}</Text>
+      <Text style={{...styles.title, ...styles.conta}}>{title2}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleBox: {
+    marginTop: 29,
+    paddingHorizontal: 50,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 45,
+    lineHeight: 55,
+    color: '#245796',
+    fontFamily: 'Montserrat',
+  },
+  conta: {
+    fontWeight: 'bold',
+  },
+});
+
+export default FormTitle;
