00bec8ca4fdbaa24753a57869ebdc0361b37e02d
adriaanbd <adriaanbd@gmail.com>
2020-05-03 17:18:52 -0500

    WIP main styling for new patient flow

--- src/components/AddPatient/AddNewPatient.js
+++ src/components/AddPatient/AddNewPatient.js
@@ -2,12 +2,26 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import InputSwitch from './InputSwitch';
 import FormTitle from '../Common/FormTitle';
+import StatusBar from './StatusBar';
+import Spacer from '../Spacer';
+import {Icon} from 'react-native-elements';
+import {navigate} from '../../utils/navigationRef';
 
 const AddNewPatient = () => {
   return (
     <View style={styles.container}>
+      <StatusBar />
       <FormTitle title1="Criar novo" title2="Paciente" />
+      <Spacer space={140} />
       <InputSwitch />
+      <Spacer space={210} />
+      <Icon
+        name="cancel"
+        type="material"
+        size={32}
+        color="#245796"
+        onPress={() => navigate('HomeScreen')}
+      />
     </View>
   );
 };
@@ -15,7 +29,8 @@ const AddNewPatient = () => {
 const styles = StyleSheet.create({
   container: {
     height: '100%',
-    width: '100%',
+    paddingHorizontal: 40,
+    alignItems: 'center',
   },
 });
 
